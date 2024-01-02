b45a83e7718e32011628aa7c7569b78aaac1cf97
manon-rittling <manon.rittling@laplateforme.io>
2024-01-02 10:14:22 +0100

    job02 fini

---
+++ job02.py
@@ -0,0 +1,9 @@
+class operation:
+    def __init__(self, nombre1, nombre2):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+
+afficher = operation(12,3)
+
+print ("le nombre 1 est: ", afficher.nombre1)
+print ("le nombre 2 est: ", afficher.nombre2)
